5ad0a4f365f0f052fdd2d5af9746c3b001b590b6
ebagabee <gh.alvesantos@gmail.com>
2026-01-17 18:45:44 -0300

    feat: add a raise exception if don't find the usage_metadata
    - print the prompt token and response token to final user

--- main.py
+++ main.py
@@ -16,4 +16,10 @@ response = client.models.generate_content(
     contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 )
 
+if response.usage_metadata is None:
+    raise RuntimeError("Failed to get the metadata")
+
+print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
+print("Response tokens: ", response.usage_metadata.candidates_token_count)
+
 print(response.text)
