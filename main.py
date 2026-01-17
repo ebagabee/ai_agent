7b2e67887ea873d9ba228d2a77d5bab78067cad3
ebagabee <gh.alvesantos@gmail.com>
2026-01-17 19:12:14 -0300

    feat: create a list of messages

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 
@@ -15,10 +16,11 @@ client = genai.Client(api_key=api_key)
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
 args = parser.parse_args()
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
 response = client.models.generate_content(
     model='gemini-2.5-flash', 
-    contents=args.user_prompt
+    contents=messages
 )
 
 if response.usage_metadata is None:
